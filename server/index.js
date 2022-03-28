7fd3e7e04db7f63e85ef2453aba2dd9fbd36a56c
Vasyl Hryts <vasylhryts@knu.ua>
2022-03-28 20:54:53 +0300

    connect to mongo, schema User

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log('Servers Hello');
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('serverPort')
+const start = async () => {
+  try {
+    mongoose.connect(config.get('dbUrl'))
+    app.listen(PORT, () => {
+      console.log('Server has been started on port', PORT)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()

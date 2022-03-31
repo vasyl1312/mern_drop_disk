4d985f3c4d0f9258d654dbccde62a1ea68f72d60
Vasyl Hryts <vasylhryts@knu.ua>
2022-03-31 20:10:47 +0300

    add dir creatind and file model

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,6 +6,8 @@ const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator')
 const router = new Router()
 const authMiddleware = require('../middleware/auth.middleware')
+const fileService = require('../services/fileService')
+const File = require('../models/File')
 
 router.post(
   '/registration',
@@ -29,7 +31,8 @@ router.post(
       }
       const hashPassword = await bcrypt.hash(password, 8)
       const user = new User({ email, password: hashPassword })
-      await user.save()
+      await user.save() //після збереження користувача в базі створюємо окрему папку для нього
+      await fileService.createDir(new File({ user: user.id, name: '' }))
       res.json({ message: 'User was created' })
     } catch (e) {
       console.log(e)

fd2e99a93669b7bc056e8084e88f4f1c4ab9a16b
Vasyl Hryts <vasylhryts@knu.ua>
2022-04-04 20:06:03 +0300

    upload file to server

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,4 +1,6 @@
 const fileService = require('../services/fileService')
+const config = require('config')
+const fs = require('fs')
 const User = require('../models/User')
 const File = require('../models/File')
 
@@ -32,6 +34,54 @@ class FileController {
     } catch (e) {
       console.log(e)
       return res.status(500).json({ message: 'Can not get files' })
+    }
+  }
+
+  async uploadFile(req, res) {
+    try {
+      const file = req.files.file
+
+      const parent = await File.findOne({ user: req.user.id, _id: req.body.parent })
+      const user = await User.findOne({ _id: req.user.id })
+      //чи є вільне місце на диску
+      if (user.usedSpace + file.size > user.diskSpace) {
+        return res.status(400).json({ message: 'There no space on the disk' })
+      }
+
+      user.usedSpace = user.usedSpace + file.size
+
+      let path
+      //якщо є батьківська папка то треба її вказати в шляху
+      if (parent) {
+        path = `${config.get('filePath')}\\${user._id}\\${parent.path}\\${file.name}`
+      } else {
+        path = `${config.get('filePath')}\\${user._id}\\${file.name}`
+      }
+
+      //чи існує вже такий файл на такому шляху
+      if (fs.existsSync(path)) {
+        return res.status(400).json({ message: 'File already exist' })
+      }
+      file.mv(path)
+
+      const type = file.name.split('.').pop() //розділюємо шлях по крапках і з цього масиву берем останній елем
+      const dbFile = new File({
+        name: file.name,
+        type,
+        size: file.size,
+        path: parent?.path,
+        parent: parent?._id,
+        user: user._id,
+      })
+
+      //збереження файлу і полів цього користувача
+      await dbFile.save()
+      await user.save()
+
+      res.json(dbFile)
+    } catch (e) {
+      console.log(e)
+      return res.status(500).json({ message: 'Upload error' })
     }
   }
 }
--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose')
 const config = require('config')
 const authRouter = require('./routes/auth.routes')
 const fileRouter = require('./routes/file.routes')
+const fileUpload = require('express-fileupload')
 const app = express()
 const PORT = config.get('serverPort')
 const corsMiddleware = require('./middleware/cors.middleware')
 
+app.use(fileUpload({}))
 app.use(corsMiddleware)
 app.use(express.json()) //бо експрес не може розпарсити json рядок
 app.use('/api/auth', authRouter)

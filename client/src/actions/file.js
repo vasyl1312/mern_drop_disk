86b38e34a591fb8e9ed498b08bd8a791cea56ad3
Vasyl Hryts <vasylhryts@knu.ua>
2022-04-05 19:35:31 +0300

    download file from server

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -48,21 +48,27 @@ export function uploadFile(file, dirId) {
       }
       const response = await axios.post(`http://localhost:5000/api/files/upload`, formData, {
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-        // onUploadProgress: (progressEvent) => {
-        //   const totalLength = progressEvent.lengthComputable
-        //     ? progressEvent.total
-        //     : progressEvent.target.getResponseHeader('content-length') ||
-        //       progressEvent.target.getResponseHeader('x-decompressed-content-length')
-        //   console.log('total', totalLength)
-        //   if (totalLength) {
-        //     let progress = Math.round((progressEvent.loaded * 100) / totalLength)
-        //     console.log(progress)
-        //   }
-        // },
       })
       dispatch(addFile(response.data))
     } catch (e) {
       alert(e.response.data.message)
     }
+  }
+}
+
+export async function downloadFile(file) {
+  const response = await fetch(`http://localhost:5000/api/files/download?id=${file._id}`, {
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+  })
+  if (response.status === 200) {
+    //запутане трохи
+    const blob = await response.blob()
+    const downloadURL = window.URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.href = downloadURL
+    link.download = file.name
+    document.body.appendChild(link)
+    link.click()
+    link.remove()
   }
 }
--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -4,6 +4,7 @@ import dirLogo from '../../../../assets/img/dir.svg'
 import fileLogo from '../../../../assets/img/file.svg'
 import { useDispatch, useSelector } from 'react-redux'
 import { pushToStack, setCurrentDir } from '../../../../reducers/fileReducer'
+import { downloadFile } from '../../../../actions/file'
 const File = ({ file }) => {
   const dispatch = useDispatch()
   const currentDir = useSelector((state) => state.files.currentDir)
@@ -15,12 +16,23 @@ const File = ({ file }) => {
     }
   }
 
+  function downloadHandler(e) {
+    e.stopPropagation()
+    downloadFile(file)
+  }
+
   return (
     <div className="file" onClick={() => openDirHandler(file)}>
       <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img" />
       <div className="file__name">{file.name}</div>
       <div className="file__date">{file.date.slice(0, 10)}</div>
       <div className="file__size">{file.size}</div>
+      {file.type !== 'dir' && (
+        <button onClick={(e) => downloadHandler(e)} className="file_btn file_download">
+          download
+        </button>
+      )}
+      <button className="file_btn file_delete">delete</button>
     </div>
   )
 }

eb1bc4fc691c156d181dc1b3a4cddbee3691c6f3
Vasyl Hryts <vasylhryts@knu.ua>
2022-04-06 19:57:58 +0300

    add how many % of download file are uploaded now

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -5,6 +5,7 @@ import FileList from './fileList/FileList'
 import './disk.css'
 import Popup from './Popup'
 import { setCurrentDir, setPopupDisplay } from '../../reducers/fileReducer'
+import Uploader from './uploader/Uploader'
 
 const Disk = () => {
   const dispatch = useDispatch()
@@ -81,6 +82,7 @@ const Disk = () => {
       </div>
       <FileList />
       <Popup />
+      <Uploader />
     </div>
   ) : (
     <div

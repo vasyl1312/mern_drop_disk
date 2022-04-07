604c0fed4368c0c05b472b2c5447cd6235b94af5
Vasyl Hryts <vasylhryts@knu.ua>
2022-04-07 17:48:32 +0300

    add animation and group by Gb, Mb, Kb, B

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -5,6 +5,7 @@ import fileLogo from '../../../../assets/img/file.svg'
 import { useDispatch, useSelector } from 'react-redux'
 import { pushToStack, setCurrentDir } from '../../../../reducers/fileReducer'
 import { deleteFile, downloadFile } from '../../../../actions/file'
+import sizeFormat from '../../../../utils/sizeFormat'
 
 const File = ({ file }) => {
   const dispatch = useDispatch()
@@ -32,7 +33,7 @@ const File = ({ file }) => {
       <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img" />
       <div className="file__name">{file.name}</div>
       <div className="file__date">{file.date.slice(0, 10)}</div>
-      <div className="file__size">{file.size}</div>
+      <div className="file__size">{sizeFormat(file.size)}</div>
       {file.type !== 'dir' && (
         <button onClick={(e) => downloadClickHandler(e)} className="file_btn file_download">
           download

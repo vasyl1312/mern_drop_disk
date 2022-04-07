8f37db40db3c764f6cb49652f1f97dc5447bf5d6
Vasyl Hryts <vasylhryts@knu.ua>
2022-04-07 19:54:23 +0300

    some bugs

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -79,14 +79,16 @@ export function uploadFile(file, dirId) {
 
 export async function downloadFile(file) {
   const response = await fetch(`http://localhost:5000/api/files/download?id=${file._id}`, {
-    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
   })
   if (response.status === 200) {
     //запутане трохи
     const blob = await response.blob()
-    const downloadURL = window.URL.createObjectURL(blob)
+    const downloadUrl = window.URL.createObjectURL(blob)
     const link = document.createElement('a')
-    link.href = downloadURL
+    link.href = downloadUrl
     link.download = file.name
     document.body.appendChild(link)
     link.click()
--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -35,11 +35,12 @@ const File = ({ file }) => {
       <div className="file__date">{file.date.slice(0, 10)}</div>
       <div className="file__size">{sizeFormat(file.size)}</div>
       {file.type !== 'dir' && (
-        <button onClick={(e) => downloadClickHandler(e)} className="file_btn file_download">
+        <button onClick={(e) => downloadClickHandler(e)} className="file__btn file__download">
           download
         </button>
       )}
-      <button onClick={(e) => deleteClickHandler(e)} className="file_btn file_delete">
+
+      <button onClick={(e) => deleteClickHandler(e)} className="file__btn file__delete">
         delete
       </button>
     </div>
